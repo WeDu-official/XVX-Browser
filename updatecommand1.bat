6f1e653a65f316590e949d7ab740b0afc3f71de5
WeDu <fplu.the.founder@gmail.com>
2024-10-31 18:30:28 +0300

    Update updatecommand1.bat

--- updatecommand1.bat
+++ updatecommand1.bat
@@ -1,11 +1,1 @@
-setlocal enabledelayedexpansion
 
-:move_file
-move "C:\Users\Haydar\Downloads\XVXBROWSERUPD.pak"  "%~dp0"
-if errorlevel 1 (
-    echo "Move failed. Retrying..."
-    goto move_file
-) else (
-    python update.py
-    echo "File moved successfully."
-)
